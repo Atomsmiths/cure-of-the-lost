15691b031c5e845ced5312a967ad62a3048484a3
Fuzznimp <montaigne.anthony.dev@gmail.com>
2023-03-18 12:01:59 +0100

    init styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import Head from "next/head";
+import Link from "next/link";
+
+import styles from "@styles/design-system.module.css";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Curse of the lost</title>
+        <title>Cure of the lost</title>
         <meta
           name="description"
           content="The Binding of Isaac tool to cartography the map."
@@ -12,7 +15,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello World</main>
+      <main>
+        <h1 className={styles["homepage-title"]}>CURE OF THE LOST</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link className={styles["menu-item"]} href="/cartography">
+                Get started!
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </main>
     </>
   );
 }
