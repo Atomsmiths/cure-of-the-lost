fb0322aa3cfd27eec2743ec5f6fcf58c8614f188
Fuzznimp <montaigne.anthony.dev@gmail.com>
2023-03-18 23:16:51 +0100

    refactor(cartography): grid generation + css

--- src/pages/cartography/index.tsx
+++ src/pages/cartography/index.tsx
@@ -1,188 +1,45 @@
-import styles from "@styles/design-system.module.css";
+import { MAP_GRID, MAP_GRID_ITEM, MAP_EMPTY } from "@pages/index.classnames";
+import { useMatrix } from "../hooks/useMatrix";
 import {
-  MAP_GRID,
-  MAP_GRID_ITEM,
-  MAP_SHOP,
-  MAP_EMPTY,
-} from "@pages/index.classnames";
+  CARTOGRAPHY_MAIN_CLASSNAMES,
+  CARTOGRAPHY_RESET_BTN_CLASSNAMES,
+  CARTOGRAPHY_TITLE_CLASSNAMES,
+} from "./index.classnames";
 
 const Cartography = () => {
-  return (
-    <div>
-      <h1>Cartography</h1>
+  const { matrix, updateMatrix, resetMatrix } = useMatrix();
 
+  function getCellClassnames(cell: string | null) {
+    if (cell === "clicked") {
+      return MAP_EMPTY;
+    }
+
+    return MAP_GRID_ITEM;
+  }
+
+  return (
+    <main className={CARTOGRAPHY_MAIN_CLASSNAMES}>
+      <h1 className={CARTOGRAPHY_TITLE_CLASSNAMES}>Cartography</h1>
       <div className={MAP_GRID}>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_SHOP}></div>
-        <div className={MAP_EMPTY}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
-        <div className={MAP_GRID_ITEM}></div>
+        {matrix.map((row, rowIndex) => {
+          return row.map((cell, colIndex) => {
+            return (
+              <div
+                key={`${rowIndex}-${colIndex}`}
+                onClick={() => updateMatrix(rowIndex, colIndex)}
+                className={getCellClassnames(cell)}
+              />
+            );
+          });
+        })}
       </div>
-    </div>
+      <button
+        onClick={resetMatrix}
+        className={CARTOGRAPHY_RESET_BTN_CLASSNAMES}
+      >
+        RESET
+      </button>
+    </main>
   );
 };
 
