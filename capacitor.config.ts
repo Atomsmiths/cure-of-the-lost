376fab25df8fe85956a5779cce69b6e18b9cc566
Fuzznimp <montaigne.anthony.dev@gmail.com>
2023-03-18 10:16:49 +0100

    chore(global): init project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "dev.cureofthelost.app",
+  appName: "cure-of-the-lost",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
