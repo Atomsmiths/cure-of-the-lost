382664833d13a3bf9b712f846bf244a95962750a
Oluane <rochepauline6@gmail.com>
2023-03-19 14:30:11 +0100

    feat(layout): WIP

--- src/pages/cartography/index.classnames.ts
+++ src/pages/cartography/index.classnames.ts
@@ -3,3 +3,5 @@ import s from "@styles/design-system.module.css";
 export const CARTOGRAPHY_TITLE_CLASSNAMES = s["cartography-title"];
 export const CARTOGRAPHY_MAIN_CLASSNAMES = s["cartography-main"];
 export const CARTOGRAPHY_RESET_BTN_CLASSNAMES = s["cartography-reset-btn"];
+export const CARTOGRAPHY_CONTAINER = s["cartography-container"];
+export const CARTOGRAPHY_MENU = s["cartography-menu"];
--- src/pages/cartography/index.tsx
+++ src/pages/cartography/index.tsx
@@ -4,6 +4,8 @@ import {
   CARTOGRAPHY_MAIN_CLASSNAMES,
   CARTOGRAPHY_RESET_BTN_CLASSNAMES,
   CARTOGRAPHY_TITLE_CLASSNAMES,
+  CARTOGRAPHY_CONTAINER,
+  CARTOGRAPHY_MENU,
 } from "./index.classnames";
 
 const Cartography = () => {
@@ -19,26 +21,31 @@ const Cartography = () => {
 
   return (
     <main className={CARTOGRAPHY_MAIN_CLASSNAMES}>
-      <h1 className={CARTOGRAPHY_TITLE_CLASSNAMES}>Cartography</h1>
-      <div className={MAP_GRID}>
-        {matrix.map((row, rowIndex) => {
-          return row.map((cell, colIndex) => {
-            return (
-              <div
-                key={`${rowIndex}-${colIndex}`}
-                onClick={() => updateMatrix(rowIndex, colIndex)}
-                className={getCellClassnames(cell)}
-              />
-            );
-          });
-        })}
+      <div className={CARTOGRAPHY_CONTAINER}>
+        <h1 className={CARTOGRAPHY_TITLE_CLASSNAMES}>Cartography</h1>
+        <div className={MAP_GRID}>
+          {matrix.map((row, rowIndex) => {
+            return row.map((cell, colIndex) => {
+              return (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  onClick={() => updateMatrix(rowIndex, colIndex)}
+                  className={getCellClassnames(cell)}
+                />
+              );
+            });
+          })}
+        </div>
       </div>
-      <button
-        onClick={resetMatrix}
-        className={CARTOGRAPHY_RESET_BTN_CLASSNAMES}
-      >
-        RESET
-      </button>
+      <div className={CARTOGRAPHY_MENU}>
+        {" "}
+        <button
+          onClick={resetMatrix}
+          className={CARTOGRAPHY_RESET_BTN_CLASSNAMES}
+        >
+          RESET
+        </button>
+      </div>
     </main>
   );
 };
